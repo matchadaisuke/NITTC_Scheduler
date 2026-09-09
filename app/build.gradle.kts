061b956cadff4a16a220879e577d05d5c6faee8b
matchadaisuke <matchadaisuke@gmail.com>
2026-09-09 20:46:44 +0900

    test: cover schedule and notification customization

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -375,6 +375,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.8.1")
 
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.json:json:20260814")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2026.06.00"))
